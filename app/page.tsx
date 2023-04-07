971cf25ab661e861a746eb8a4bacb99b341d5624
midCenturyDude7 <mgriffes@gmail.com>
2023-04-06 17:31:19 -0700

    Developed very basic, working Next.js app with routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
+import Link from "next/link";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next.js Starter App",
+  description: "Welcome to my Next.js starter app",
+  viewport: "width=device-width, initial-scale=1",
+};
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <h1>Hello, World!</h1>
+      <Link href="/about">About page</Link>
     </main>
   );
 }
